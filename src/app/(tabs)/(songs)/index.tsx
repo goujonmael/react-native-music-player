0fc41170dad40d0df2d44becff5a48a40dbf94cc
Mig <goujonmael@gmail.com>
2024-07-27 18:32:17 +0200

    padding

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -1,11 +1,14 @@
 import { TrackList } from '@/components/TrackList'
+import { screenPadding } from '@/constants/tokens'
 import { defaultStyles } from '@/styles'
 import { View, Text, ScrollView } from 'react-native'
 
 const Songscreen = () => {
 	return (
 		<View style={defaultStyles.container}>
-			<ScrollView>
+			<ScrollView
+			contentInsetAdjustmentBehavior='automatic'
+			style={{paddingHorizontal: screenPadding.horizontal}}>
 				<TrackList scrollEnabled={false}/>
 			</ScrollView>
 		</View>
